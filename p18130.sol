0fa587028fc29e21f63ea7b4da22b72f23008061
PrekWood <46693693+PrekWood@users.noreply.github.com>
2022-07-11 00:11:42 +0300

    Update p18130.sol

--- p18130.sol
+++ p18130.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
 import "hardhat/console.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
 
 interface IERC20 {
     function totalSupply() external view returns (uint256);
