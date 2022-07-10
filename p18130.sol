1781079c3027c44c4f26c9fa216d72561baedfca
PrekWood <46693693+PrekWood@users.noreply.github.com>
2022-07-11 00:11:31 +0300

    Update p18130.sol

--- p18130.sol
+++ p18130.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 interface IERC20 {
     function totalSupply() external view returns (uint256);
@@ -15,54 +16,59 @@ interface IERC20 {
     event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
-contract UnipiContract is IERC20 {
+contract UnipiContract is IERC20 { 
 
     address payable owner;
+    address payable store;
     mapping (address => bool) admins;
     AdminChangeState adminChangeState;
-    mapping (uint => Product) products;
-    mapping (address => Order) orders;
+    Order[] orders;
     uint256 balance;
     uint256 pendingBalance;
+    WithdrawRequest[] withdrawRequests;
 
     // ERC-20 Token properties
     string name = "UnipiToken";
     string symbol = "UT";
     uint256 decimals = 18;
-    uint256 totalTokenSupply = 1000000000000000000000000;
+    uint256 totalTokenSupply = 1000000000000000000000000; 
     uint256 ethereumExchangeRate = 90; // 1 wei = 90 MST
     mapping(address => uint256) userBalances;
     mapping(address => mapping (address => uint256)) accountsWithAccess;
 
-    struct Product{
-        uint id;
-        string name;
-        uint price;
-        bool exists;
-    }
     struct AdminChangeState{
-        bool exists;
+        bool exists; 
         address oldAdmin;
         address newAdmin;
     }
     struct Order{
+        uint256 id;
+        uint256 amount;
         address customer;
-        uint idProduct;
         OrderState state;
-        bool exists;
-        address[] approvedAdmins;
+        bool exists; 
     }
-    enum OrderState { PENDING, COMPLETED, CANCELED }
+    enum OrderState {
+        PENDING, 
+        COMPLETED,
+        CANCELED 
+    }
+    struct WithdrawRequest{
+        bool exists; 
+        uint256 id;
+        address to;
+        uint256 amount;
+        address[] adminsApproved;
+    }
 
     constructor() {
         owner = payable(msg.sender);
+
         admins[0x5B38Da6a701c568545dCfcB03FcB875f56beddC4] = true;
         admins[0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db] = true;
         admins[0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2] = true;
 
-        products[0] = Product(1,"Product #1", 700, true);
-        products[1] = Product(2,"Product #2", 10000, true);
-        products[2] = Product(3,"Product #3", 200000, true);
+        store = payable(0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB);
 
         userBalances[owner] = totalTokenSupply;
     }
@@ -78,11 +84,7 @@ contract UnipiContract is IERC20 {
     // Admin Change
     function adminChange(address newAdminAddress) public onlyAdmin {
         require(!adminChangeState.exists, "Another admin change is currently running.");
-
-        adminChangeState.exists = true;
-        adminChangeState.oldAdmin = msg.sender;
-        adminChangeState.newAdmin = newAdminAddress;
-        adminChangeState.exists = true;
+        adminChangeState = AdminChangeState(true, msg.sender, newAdminAddress);
     }
     function adminChangeApprove() public onlyAdmin{
         require(msg.sender != adminChangeState.oldAdmin, "You cannot approve your own account change.");
@@ -95,63 +97,152 @@ contract UnipiContract is IERC20 {
         adminChangeState.exists = false;
     }
 
-    // Client
-    function orderCreate(uint idProduct) payable public {
-        require(products[idProduct].exists, "Invalid id product");
+    // Order 
+    function orderCreate(uint idOrder, uint256 amount) public {
 
-        // Add ETH of the request to the user balance
-        uint256 unipiTokenValue = msg.value*ethereumExchangeRate;
-        userBalances[msg.sender] += unipiTokenValue;
+        require(userBalances[msg.sender] >= amount, "Your balance is not enough to pay the order");
 
-        // Check if balance is enough
-        require(userBalances[msg.sender] >= products[idProduct].price, "Your balance is not enough to buy the product");
+        // Search if idOrder is unique
+        Order memory order;
+        for(uint orderIndex = 0; orderIndex < orders.length; orderIndex++){
+            if(orders[orderIndex].id == idOrder){
+                order = orders[orderIndex];
+                break;
+            }
+        }
+        require(!order.exists, "The idOrder is not unique");
 
         // Reserve the tokens
-        userBalances[msg.sender] -= products[idProduct].price;
-        pendingBalance += products[idProduct].price;
+        userBalances[msg.sender] -= amount;
+        pendingBalance += amount;
 
         // Crate order
-        address[] memory emptyAddressArray;
-        orders[msg.sender] = Order(
-            msg.sender,
-            idProduct,
-            OrderState.PENDING,
-            true,
-            emptyAddressArray
+        orders.push(
+            Order(
+                idOrder,
+                amount,
+                msg.sender, 
+                OrderState.PENDING, 
+                true
+            )
         );
     }
-    function orderApprove(address customerAddress) public onlyAdmin{
-        require(orders[customerAddress].exists, "The customer hasn't made any orders yet");
-        require(customerAddress != msg.sender, "You cannot approve your own order");
+    function orderApprove(uint idOrder) public {
+        require(msg.sender == store, "You don't have authority to approve orders");
+
+        // Search for order
+        Order memory order;
+        for(uint orderIndex = 0; orderIndex < orders.length; orderIndex++){
+            if(orders[orderIndex].id == idOrder){
+                order = orders[orderIndex];
+                break;
+            }
+        }
+        require(order.exists, "Could not find an order with this id");
+
+        order.state = OrderState.COMPLETED;
+        pendingBalance -= order.amount;
+        balance += order.amount;
+    }
+    function orderDecline(uint idOrder) public { 
+        // Search for order
+        Order memory order;
+        for(uint orderIndex = 0; orderIndex < orders.length; orderIndex++){
+            if(orders[orderIndex].id == idOrder){
+                order = orders[orderIndex];
+                break;
+            }
+        }
+        require(order.exists, "Could not find an order with this id");
+
+        // Only grant access to the store or the customer
+        require(msg.sender == store || msg.sender == order.customer, "You don't have authority to approve orders");
+        require(order.state == OrderState.PENDING, "The order is either canceled or completed");
+
+        order.state = OrderState.CANCELED;
+        pendingBalance -= order.amount;
+        userBalances[order.customer] += order.amount;
+    }
+
+    // Withdrawals
+    function withdraw(uint256 tokenAmount) public returns (uint256 idWithdrawRequest){ 
+        require(tokenAmount <= balance, "The contract currently does not have the number of tokens you want to withdraw.");
+
+        uint256 id = withdrawRequests.length + 1;
+        address[] memory emptyAddressArray;
+        withdrawRequests.push(
+            WithdrawRequest(
+                true,
+                id,
+                msg.sender,
+                tokenAmount,
+                emptyAddressArray
+            )
+        );
+
+        return id;
+    }
+    function withdrawAccept(uint256 idWithdrawRequest) public onlyAdmin returns(string memory)  { 
+        // Search for request
+        WithdrawRequest memory request;
+        uint requestPosition;
+        for(uint requestIndex = 0; requestIndex < withdrawRequests.length; requestIndex++){
+            if(withdrawRequests[requestIndex].id == idWithdrawRequest){
+                request = withdrawRequests[requestIndex];
+                requestPosition = requestIndex;
+                break;
+            }
+        }
+        require(request.exists, "Could not find a withdrawal request with this id");
 
         // Check if already approved
-        address[] storage approvedAdmins = orders[customerAddress].approvedAdmins;
-        for(uint approvedAdminIndex=0; approvedAdminIndex < approvedAdmins.length; approvedAdminIndex++){
-            require(approvedAdmins[approvedAdminIndex] != msg.sender, "You have already approved this order");
+        bool alreadyApproved = false;
+        for(uint adminIndex = 0; adminIndex < request.adminsApproved.length; adminIndex++){
+            if(request.adminsApproved[adminIndex] == msg.sender){
+                alreadyApproved = true;
+                break;
+            }
         }
-        orders[customerAddress].approvedAdmins.push(msg.sender);
+        require(!alreadyApproved, "You have already approved this withdrawal");
 
-        if(orders[customerAddress].approvedAdmins.length >= 2){
-            uint256 orderTotal  = products[orders[customerAddress].idProduct].price;
-            pendingBalance -= orderTotal;
-            balance += orderTotal;
-            orders[customerAddress].state = OrderState.COMPLETED;
+        withdrawRequests[requestPosition].adminsApproved.push(msg.sender);
+
+        if(request.adminsApproved.length >= 1){
+            require(request.amount <= balance, "The amount is not available right now.");
+            
+            // Transfer the amount
+            uint etherAmount = uint(request.amount)/uint(ethereumExchangeRate);
+            payable(request.to).transfer(etherAmount);
+
+            // Delete withdraw request
+            delete withdrawRequests[requestPosition];
+
+            return "Withdrawal completed";
+        }else{
+            return "Another one approve is needed to complete the withdrawal";
         }
     }
-    function orderDecline(address customerAddress) public{
-        require(admins[msg.sender] || msg.sender == customerAddress, "You dont have access editing the order");
-        require(orders[customerAddress].exists, "The customer hasn't made any orders yet");
-        require(orders[customerAddress].state == OrderState.PENDING, "The order cannot be canceled");
+    function withdrawDecline(uint256 idWithdrawRequest) public onlyAdmin{ 
+        // Search for request
+        WithdrawRequest memory request;
+        uint requestPosition;
+        for(uint requestIndex = 0; requestIndex < withdrawRequests.length; requestIndex++){
+            if(withdrawRequests[requestIndex].id == idWithdrawRequest){
+                request = withdrawRequests[requestIndex];
+                requestPosition = requestIndex;
+                break;
+            }
+        }
+        require(request.exists, "Could not find a withdrawal request with this id");
 
-        // The orders value goes back to the clients balance
-        uint256 orderTotal  = products[orders[customerAddress].idProduct].price;
-        pendingBalance -= orderTotal;
-        userBalances[customerAddress] += orderTotal;
-        orders[customerAddress].state = OrderState.CANCELED;
+        // Delete withdraw request
+        delete withdrawRequests[requestPosition];
     }
 
 
-    function changeEthereumExchangeRate(uint256 newExchangeRate) public{
+    // Change UnipiToken exchage rate
+    function changeUnipiTokenExchangeRate(uint256 newExchangeRate) public{
+        require(msg.sender == store, "You don't have authority to change the exchange rate");
         ethereumExchangeRate = newExchangeRate;
     }
     function buyUnipiTokens() public payable{
